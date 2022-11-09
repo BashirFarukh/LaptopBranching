70d2d363aa03326bdd283db4c4ed43f664253cd4
Bashir Farukh <bashir.farukh@tekschool.us>
2022-11-09 12:45:33 -0800

    test

--- src/gitVersionControl/GitVpractice.java
+++ src/gitVersionControl/GitVpractice.java
@@ -4,8 +4,13 @@ public class GitVpractice {
 	public static void main(String[] args) {
 		
 	}
-	String name = "FromRemote"
-	String name = "Bashir";
+	String name = "FromRemote";
+	String name1 = "Bashir";
 	int num = 2000;
+	public void Test() {
+		System.out.println("fromLocal");
+	}
+	
+	
 
 }
